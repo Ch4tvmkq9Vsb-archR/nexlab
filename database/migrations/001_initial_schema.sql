9920069d9596ee7f092d43564c568be274614eb0
Ch4tvmkq9Vsb-archR <cleiton2vieira@icloud.com>
2026-03-05 06:34:13 -0300

    Update 001_initial_schema.sql

--- database/migrations/001_initial_schema.sql
+++ database/migrations/001_initial_schema.sql
@@ -53,7 +53,7 @@ create table cc_test_counters (
  id uuid primary key default gen_random_uuid(),
  cc_id uuid references cc(id),
  test_type varchar(3) references test_types(code),
- last_seq integer default 0,
+ last_seq integer not null default 0,
  created_at timestamptz default now(),
  unique(cc_id, test_type)
 );
